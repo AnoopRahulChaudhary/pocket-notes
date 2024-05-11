4a88b97c1331d2e72c72614569ef5b2d90f2826e
Anoop Chaudhary <anooprahulchaudhary@gmail.com>
2024-05-11 09:35:46 +0530

    Added style for app-name

--- src/components/AppName/index.jsx
+++ src/components/AppName/index.jsx
@@ -1,8 +1,9 @@
 import React from "react";
+import styles from './index.module.css'
 
 function AppName(){
     return(
-        <div>
+        <div className={styles.app_name_container}>
             <span>Pocket Notes</span>
         </div>
     );
