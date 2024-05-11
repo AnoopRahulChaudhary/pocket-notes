817c7745e258af6f0e1fc060aecff4d76dfb1c17
Anoop Chaudhary <anooprahulchaudhary@gmail.com>
2024-05-11 10:51:55 +0530

    added style for add-group-button

--- src/components/NoteGroupDashboard/AddNoteGroupButton/index.jsx
+++ src/components/NoteGroupDashboard/AddNoteGroupButton/index.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import CreateNoteGroup from "../CreateNoteGroup";
 import { useDispatch, useSelector } from "react-redux";
 import { showNoteGroupCreator } from "../../../actions/createNoteGroup";
+import styles from './index.module.css'
+import pluslogo from '../../../assets/plus-lg.svg';
 
 function AddNoteGroupButton() {
     const showModal = useSelector(state => state.createNoteGroupReducer.show);
@@ -10,9 +12,9 @@ function AddNoteGroupButton() {
     return (
         <div>
             {showModal && <CreateNoteGroup />}
-            <button id="addNoteGroup"
-                    onClick={() => dispatch(showNoteGroupCreator())}>
-                Add Note Group
+            <button className={styles.add_note_group_button} id="addNoteGroup"
+                    onClick={() => dispatch(showNoteGroupCreator())}>   
+                <img src={pluslogo} alt="plus-logo"/>
             </button>
         </div>
     )
